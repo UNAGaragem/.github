136330fce4b9ad84e717dc8503b61115fd260c88
Sudário Gonçalves <sudarioogoncalves@gmail.com>
2023-09-19 04:57:22 -0300

    Codigos Iniciais JAVA do Projeto Una Garagem

---
+++ src/main/java/projunagaragem/ProjUnaGaragem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package projunagaragem;
+
+/**
+ *
+ * @author sudar
+ */
+public class ProjUnaGaragem {
+
+    public static void main(String[] args) {
+        
+         View.Login Login = new View.Login();
+         Login.setVisible(true);
+       
+    }
+}
